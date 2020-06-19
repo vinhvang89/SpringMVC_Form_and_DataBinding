f37b3127852b24921347378bf5aa1671f50930fb
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-06-19 11:18:16 +0700

    new

---
+++ src/main/java/Com/Controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package Com.Controller;
+
+import Com.Model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    @RequestMapping(value = "/showForm",method = RequestMethod.GET)
+    public String showForm(ModelMap modelMap){
+        modelMap.addAttribute("employee",new Employee());
+        return "create";
+    }
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "info";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/employee/info.jsp
@@ -0,0 +1,30 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 33vin
+  Date: 18-Jun-20
+  Time: 21:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Inform</title>
+</head>
+<body>
+<h2>Submitted Employee Information</h2>
+<table>
+    <tr>
+        <td>Name :</td>
+        <td>${name}</td>
+    </tr>
+    <tr>
+        <td>ID :</td>
+        <td>${id}</td>
+    </tr>
+    <tr>
+        <td>Contact Number :</td>
+        <td>${contactNumber}</td>
+    </tr>
+</table>
+</body>
+</html>
